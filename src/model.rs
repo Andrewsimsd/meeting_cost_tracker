fcd9683b0878fa93c67883c1f0465feac64f5ddc
Andrewsimsd <andrew.sims.d@gmail.com>
2025-07-13 11:57:25 -0400

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -130,6 +130,7 @@ impl EmployeeCategory {
     /// * [`EmployeeCategory::salary`]
     #[must_use]
     pub fn cost_per_millisecond(&self) -> f64 {
-        self.salary / (365.25 * 24.0 * 60.0 * 60.0 * 1000.0)
+        // 2000 work hours per year, 60 mins per hours, 60 secs per hour, 1000 ms per second.
+        self.salary / (2000.0 * 60.0 * 60.0 * 1000.0)
     }
 }
